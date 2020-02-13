6c823c1419ec73a523a2046a917379673f4796bb
Avani Patel <anpatel36@yahoo.in>
2020-02-12 23:47:22 -0500

    [AP] Bill class created and attributes are added in that

---
+++ C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/Bill.swift
@@ -0,0 +1,21 @@
+//
+//  Bill.swift
+//  C0772788_W2020_MID_TERM_MAD3004
+//
+//  Created by Avani Patel on 2/12/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+enum BillType
+{
+    case INTERNET, MOBILE, HYDRO
+}
+
+class Bill {
+    var billID: Int
+    var billDate: Date
+    var billType: BillType
+    var totalBillAmount: Double
+}
---
+++ C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0772788_W2020_MID_TERM_MAD3004
+//
+//  Created by Avani Patel on 2/12/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

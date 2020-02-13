244895b5a18e34262da0915b8656615287ec7190
Avani Patel <anpatel36@yahoo.in>
2020-02-13 17:42:12 -0500

    [AP] Internet class added

---
+++ C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/Internet.swift
@@ -0,0 +1,13 @@
+//
+//  Internet.swift
+//  C0772788_W2020_MID_TERM_MAD3004
+//
+//  Created by Avani Patel on 2/13/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+class Internet
+{
+    
+}

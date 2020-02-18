2539c8f0239cc19b58e7f2f22c7ffbb7d84bc16b
Avani Patel <anpatel36@yahoo.in>
2020-02-18 16:35:45 -0500

    [AP] ATTRIBUTES ADDED AND INITIALIZED IN INSURANCE CLASS

--- C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/Insurance.swift
+++ C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/Insurance.swift
@@ -7,7 +7,38 @@
 //
 
 import Foundation
-class Insurance
+
+enum InsuranceType {
+    case HOME, CAR, BUSINESS
+}
+
+class Insurance : Bill
 {
+    var insuranceProviderName: String
+    var insuranceType: InsuranceType
+    var startDate: Date
+    var endDate: Date
+    var totalDays: Int
+    var totalInstallmentToPay: Double = 0.0
+    
+    init(billID: String, billDate: Date, billType: BillType, insuranceProviderName: String, insuranceType: InsuranceType, startDate: Date, endDate: Date) {
+        self.insuranceProviderName = insuranceProviderName
+        self.insuranceType = insuranceType
+        self.startDate = startDate
+        self.endDate = endDate
+        self.totalDays = Calendar.current.dateComponents([.day], from: startDate,to: endDate).day!
+        super.init(billID: billID, billDate: billDate, billType: billType)
+    }
+    
+    override func display() {
+        super.display()
+        print("Insurance Provider Name : \(insuranceProviderName)")
+        print("Insurance Type : \(insuranceType)")
+        print("Start Date : \(startDate.getForamttedDate())")
+        print("End Date : \(endDate.getForamttedDate())")
+        print("Total Installment to Pay : \(totalInstallmentToPay.currency())")
+    }
+    
+    
     
 }

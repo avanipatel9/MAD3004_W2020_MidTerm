48ddfcf39606182146ae05692cc6925d4a81b7d5
Avani Patel <anpatel36@yahoo.in>
2020-02-18 17:54:41 -0500

    [AP] ENUM ADDED FOR ERROR HANDLING

---
+++ C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/ExceptionHandling.swift
@@ -0,0 +1,12 @@
+//
+//  ExceptionHandling.swift
+//  C0772788_W2020_MID_TERM_MAD3004
+//
+//  Created by Avani Patel on 2/18/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+enum CustomerError : Error {
+    case INVALIDEMAIL, INVALIDPHONENUMBER
+}

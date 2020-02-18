467ed8aaa4494df216e411d0825b6b10b0938591
Avani Patel <anpatel36@yahoo.in>
2020-02-18 12:32:50 -0500

    [AP] Insurance class added

---
+++ C0772788_W2020_MID_TERM_MAD3004/C0772788_W2020_MID_TERM_MAD3004/Insurance.swift
@@ -0,0 +1,13 @@
+//
+//  Insurance.swift
+//  C0772788_W2020_MID_TERM_MAD3004
+//
+//  Created by Avani Patel on 2/18/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+class Insurance
+{
+    
+}
